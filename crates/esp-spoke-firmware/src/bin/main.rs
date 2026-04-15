cycling-pov/esp32-pov-rs

commit 48cc91f27f4e373d8d822470684e4821e21bce48

esp-spoke-firmware: Expose async APIs for networking channels
Expose future for the networking APIs to avoid the need to wait in
main.rs when checking the channels. Instead, we'll just access the
queues as they report data.

crates/esp-spoke-firmware/src/bin/main.rs
@@ -9,6 +9,7 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+#[cfg(feature = "heap-stats")]
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 #[cfg(feature = "sk9822-strip")]
@@ -17,6 +18,8 @@ use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
 
+#[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
+use embassy_futures::select::{Either, select};
 use esp_hal::timer::timg::TimerGroup;
 use esp_spoke_firmware::led;
 #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
@@ -83,13 +86,20 @@ async fn main(spawner: Spawner) -> ! {
     // Forward networking events to the active LED task.
     #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
     loop {
-        if let Some(command) = networking::try_receive_command() {
-            led::try_send_led_command(LedCommand::Frame(command));
+        match select(
+            networking::receive_command(),
+            networking::receive_download(),
+        )
+        .await
+        {
+            Either::First(Some(command)) => {
+                led::try_send_led_command(LedCommand::Frame(command));
+            }
+            Either::Second(Some(download)) => {
+                led::try_send_led_command(LedCommand::Download(download));
+            }
+            _ => {}
         }
-        if let Some(download) = networking::try_receive_download() {
-            led::try_send_led_command(LedCommand::Download(download));
-        }
-        Timer::after(Duration::from_millis(25)).await;
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples

crates/esp-spoke-firmware/src/networking/mod.rs
@@ -24,12 +24,12 @@ static DOWNLOAD_CHANNEL: Channel<CriticalSectionRawMutex, Box<CompletedDownload>
     Channel::new();
 static COMMAND_CHANNEL: Channel<CriticalSectionRawMutex, CommandFrame, 4> = Channel::new();
 
-pub fn try_receive_download() -> Option<Box<CompletedDownload>> {
-    DOWNLOAD_CHANNEL.receiver().try_receive().ok()
+pub async fn receive_download() -> Option<Box<CompletedDownload>> {
+    DOWNLOAD_CHANNEL.receive().await.try_into().ok()
 }
 
-pub fn try_receive_command() -> Option<CommandFrame> {
-    COMMAND_CHANNEL.receiver().try_receive().ok()
+pub async fn receive_command() -> Option<CommandFrame> {
+    COMMAND_CHANNEL.receiver().receive().await.try_into().ok()
 }
 
 #[cfg(any(feature = "ble", feature = "espnow"))]

commit 12fc8b9d705b66e984e9f67684624c144824a53f

esp-spoke-firmware: Fix clippy warnings
Fix clippy issues introduced in 48cc91f.

crates/esp-spoke-firmware/src/bin/main.rs
@@ -21,7 +21,10 @@ extern crate alloc;
 #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
 use embassy_futures::select::{Either, select};
 use esp_hal::timer::timg::TimerGroup;
+
+#[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
 use esp_spoke_firmware::led;
+
 #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
 use esp_spoke_firmware::led::LedCommand;
 use esp_spoke_firmware::networking;
@@ -83,22 +86,24 @@ async fn main(spawner: Spawner) -> ! {
         spawner,
     );
 
-    // Forward networking events to the active LED task.
-    #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
     loop {
-        match select(
-            networking::receive_command(),
-            networking::receive_download(),
-        )
-        .await
+        // Forward networking events to the active LED task.
+        #[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
         {
-            Either::First(Some(command)) => {
-                led::try_send_led_command(LedCommand::Frame(command));
+            match select(
+                networking::receive_command(),
+                networking::receive_download(),
+            )
+            .await
+            {
+                Either::First(Some(command)) => {
+                    led::try_send_led_command(LedCommand::Frame(command));
+                }
+                Either::Second(Some(download)) => {
+                    led::try_send_led_command(LedCommand::Download(download));
+                }
+                _ => {}
             }
-            Either::Second(Some(download)) => {
-                led::try_send_led_command(LedCommand::Download(download));
-            }
-            _ => {}
         }
     }
 

crates/esp-spoke-firmware/src/led/mod.rs
@@ -5,7 +5,10 @@ mod strip;
 mod waveshare_matrix;
 
 use alloc::boxed::Box;
+
+#[cfg(any(feature = "waveshare-matrix", feature = "sk9822-strip"))]
 use embassy_executor::Spawner;
+
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
 #[cfg(feature = "sk9822-strip")]

crates/esp-spoke-firmware/src/networking/mod.rs
@@ -25,11 +25,11 @@ static DOWNLOAD_CHANNEL: Channel<CriticalSectionRawMutex, Box<CompletedDownload>
 static COMMAND_CHANNEL: Channel<CriticalSectionRawMutex, CommandFrame, 4> = Channel::new();
 
 pub async fn receive_download() -> Option<Box<CompletedDownload>> {
-    DOWNLOAD_CHANNEL.receive().await.try_into().ok()
+    DOWNLOAD_CHANNEL.receive().await.into()
 }
 
 pub async fn receive_command() -> Option<CommandFrame> {
-    COMMAND_CHANNEL.receiver().receive().await.try_into().ok()
+    COMMAND_CHANNEL.receiver().receive().await.into()
 }
 
 #[cfg(any(feature = "ble", feature = "espnow"))]